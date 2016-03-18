e75e13daff9ac0d500da1267810154e38b44fbca
kikest <enguike@hotmail.es>
2016-03-18 17:31:16 +0100

    Added the use example of lda studied in our lessons

--- Assignment2/reduce_dim.py
+++ Assignment2/reduce_dim.py
@@ -1,6 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 import numpy as np 
 from scipy.linalg import eigh 
+
+#for tests
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
+from sklearn import lda
+
 
 class LDA:
     
@@ -17,3 +27,38 @@ class PCA:
 
     def transform(self, X):
         pass #incluye aquí tu código
+
+if __name__ == "__main__" :
+
+    FisherMulticlase = LDA()
+    "Pruebas de lda"
+    db = datasets.load_digits()
+
+    db.keys()
+
+    db.data[0].shape
+
+    db.images[0]
+
+    db.data[0]
+
+    plt.imshow(db.images[0], cmap='gray')
+
+    fisher_LDA = lda.LDA(n_components=2)
+
+    X, y = db.data, db.target
+
+    fisher_LDA.fit(X, y)
+
+    X_reduced = fisher_LDA.transform(X)
+
+    X_reduced.shape
+
+    plt.plot(X_reduced[:, 0], X_reduced[:, 1], 'o')
+
+    y == 7
+
+    for k in [1, 8, 0]:
+        plt.plot(X_reduced[y == k, 0], X_reduced[y == k, 1], 'o')
+
+    plt.show()
