b484b57ae67d146b153b80b4bb99d27e62d97835
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2016-04-08 17:33:17 +0200

    Possible final version. Autopep8 done. Still T5 on FLOP.

--- Assignment2/reduce_dim.py
+++ Assignment2/reduce_dim.py
@@ -27,7 +27,7 @@ class LDA:
     w : ndarray[float]
         Projection matrix.
     mean : float
-        Mean of X_train points.
+        Vectorial mean of X_train points.
 
     Methods
     -------
@@ -43,31 +43,29 @@ class LDA:
         self.mean = None
 
     def fit(self, X_train, y_train, reduced_dim):
-        """Computes the projection matrix w for lda.
+        """Compute w, the projection matrix for LDA.
 
         Parameters
         ----------
         X_train
             Set of training points.
         y_train
-            Vector of classes of X_train points.
+            Vector of classes of the points given by X_train.
         reduced_dim
-            Target dimension of the reduction.
+            Target dimension of the dimensionality reduction.
 
         Examples
         --------
             >>> my_lda = LDA()
         Introduce some values for X_train and y_train:
-            >>>> X_train = np.asarray([[34.12, 31.05, -5.98], 
-              [-17.21, -28.20, 17.54], [36.76, 26.94, -6.18], 
-              [-37.79, -1.14, -1.370], [34.65, 29.18, -6.29], 
-              [-18.57, 28.660, 18.45]])
+            >>> X_train = np.asarray([[34.12, 31.05, -5.98], [-17.21, -28.20, 17.54],
+                                      [36.76, 26.94, -6.18], [-37.79, -1.14, -1.370],
+                                      [34.65, 29.18, -6.29], [-18.57, 28.660, 18.45]])
             >>> y_train = np.asarray([0, 1, 2, 2, 1, 0])
-        Choice the target dimension of reduction:
+        Choose the target dimension of reduction:
             >>> reduced_dim = 2
             >>> my_lda.fit(X_train, y_train, reduced_dim)
-
-        The projecting matrix resulting:
+        Then w will be:
             >>> [[-0.00325751 -0.02306827]
                  [ 0.0325941   0.0152601 ]
                  [ 0.04380306 -0.05359897]]
@@ -98,8 +96,7 @@ class LDA:
         #self.w = evecs[:, :reduced_dim]
 
     def transform(self, X):
-        """Project the points of X using the
-        w matrix calculated with fit
+        """Project the points of X using w, which was calculated with fit.
 
         Parameters
         ----------
@@ -109,17 +106,16 @@ class LDA:
         Returns:
         ----------
         X_projected
-            Projected points using the w.
+            Projected points using w.
 
         Examples
         --------
-        Introduce some values for X:
-            >>> X = np.asarray([[-36.61, -0.95, -27.53], 
-                                [34.06, 25.760, -5.190], [-16.56, -28.34, 19.55], 
-                                [-16.71, -27.70, 18.27], [-36.91, -0.46, -26.70], 
+        Assuming w is already computed, introduce some values for X:
+            >>> X = np.asarray([[-36.61, -0.95, -27.53],
+                                [34.06, 25.760, -5.190], [-16.56, -28.34, 19.55],
+                                [-16.71, -27.70, 18.27], [-36.91, -0.46, -26.70],
                                 [-36.65, -3.62, -23.23], [-18.72, 28.57, 19.04]])
             >>> X_projected = my_lda.transform(X)
-
         X_projected:
             >>> [[-1.68814683  2.35296383]
                  [-0.06920616 -0.06707466]
@@ -169,7 +165,7 @@ class PCA:
         self.mean = None
 
     def fit(self, X_train, reduced_dim):
-        """Compute the projection matrix w for PCA.
+        """Compute w, the projection matrix for PCA.
 
         Parameters
         ----------
@@ -182,24 +178,22 @@ class PCA:
         --------
             >>> my_pca = PCA()
         Introduce some values for X_train:
-            >>> X_train = np.asarray([[34.12, 31.05, -5.98], [-17.21, -28.20, 17.54], 
-                                      [36.76, 26.94, -6.18], [-37.79, -1.14, -1.370], 
+            >>> X_train = np.asarray([[34.12, 31.05, -5.98], [-17.21, -28.20, 17.54],
+                                      [36.76, 26.94, -6.18], [-37.79, -1.14, -1.370],
                                       [34.65, 29.18, -6.29], [-18.57, 28.66, 18.450]])
-
+        Choose the target dimension of reduction:
             >>> reduced_dim = 2
             >>> my_pca.fit(X_train, reduced_dim)
         The projecting matrix w:
             >>> [[ 0.83558706  0.50419046]
                  [ 0.50454667 -0.86141214]
                  [-0.21731757 -0.06132779]]
-
-        With other values for X_train:
-            >>> X_train = np.asarray([[34.12, 31.05, -5.98,-17.21, -28.20], 
-                                      [36.76, 26.940, -6.18, -1.14, -1.37], 
+        With some other values for X_train:
+            >>> X_train = np.asarray([[34.12, 31.05, -5.98,-17.21, -28.20],
+                                      [36.76, 26.940, -6.18, -1.14, -1.37],
                                       [34.65, 29.180, -6.29, 28.66, 18.45]])
-
             >>> my_pca.fit(X_train, reduced_dim)
-        The projecting matrix w:
+        The new w will be:
             >>> [[ 0.00642278  0.28546226]
                  [-0.02630362 -0.38638449]
                  [-0.00469732 -0.00663974]
@@ -207,15 +201,20 @@ class PCA:
                  [ 0.70915674  0.60951702]]
 
         """
+        # Compute the dimension of the training points
+        D = np.shape(X_train)[1]
+        # Compute and save the vectorial mean for latter purposes
         self.mean = np.mean(X_train, axis=0)
+        # Compute the covariance matrix of X_train
         St = np.cov(X_train.T, bias=1) * np.shape(X_train)[0]
-        evecs = eigh(St)[1]
-        evecs = np.fliplr(evecs)
-        self.w = evecs[:, :reduced_dim]
+        # Compute eigenvalues for PCA problem and sort them in a decreasing fashion
+        self.w = np.fliplr(eigh(St, eigvals = (D - reduced_dim, D - 1))[1])
+        #evecs = eigh(St)[1]
+        #evecs = np.fliplr(evecs)
+        #self.w = evecs[:, :reduced_dim]
 
     def transform(self, X):
-        """Project the points of X using
-        w matrix, calculated with fit.
+        """Project the points of X using w, which was calculated with fit.
 
         Parameters
         ----------
@@ -225,13 +224,13 @@ class PCA:
         Returns:
         ----------
         X_projected
-            Projected points using the w.
+            Projected points using w.
 
         Examples
         --------
-        Introduce some values for X:
-            >>> X = np.asarray([[-36.61, -0.95, -27.53], [34.060, 25.760, -5.19], 
-                                [-16.56, -28.34, 19.55], [-16.71, -27.70, 18.27], 
+        Assuming w is already computed, introduce some values for X:
+            >>> X = np.asarray([[-36.61, -0.95, -27.53], [34.060, 25.760, -5.19],
+                                [-16.56, -28.34, 19.55], [-16.71, -27.70, 18.27],
                                 [-36.91, -0.46, -26.70], [-36.65, -3.62, -23.23],
                                 [-18.72, 28.570, 19.04]])
             >>> X_projected = my_pca.transform(X)
@@ -244,7 +243,7 @@ class PCA:
                  [-38.54070049  -4.03874396]
                  [-16.50328106 -25.31979152]]
         Now with a different X:
-            >>> X = np.asarray([[-36.61, -0.950, -27.53, 25.76, -5.19], 
+            >>> X = np.asarray([[-36.61, -0.950, -27.53, 25.76, -5.19],
                                 [-16.56, -28.34, 19.55, -27.70, 18.27]])
             >>> X_projected = my_pca.transform(X)
         X_projected will be:
