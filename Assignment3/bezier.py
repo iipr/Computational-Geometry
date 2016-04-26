d31248723f82701d7dc8a97dcb5f95ced4c4d78b
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2016-04-26 16:07:06 +0200

    Added (good) Horner and minor changes

--- Assignment3/bezier.py
+++ Assignment3/bezier.py
@@ -15,17 +15,17 @@ RECURSIVE_BERNSTEIN_DICT = dict()
        'recursive' -> los polinomios de Bernstein se calculen usando la fórmula recursiva que los caracteriza 
        'horner' ->  Horner para evaluar, dividiendo los valores en los menores que 0.5 y los mayores o iguales a 0.5
        'deCasteljau' -> algoritmo de De Casteljau
-    
+
     Devolverá un np.array de dimensión (num_points, dim) con los valores de la curva de Bézier en los 
     instantes dados por num_points valores equiespaciados en [0, 1].
     '''
-    
+
 def comb(m, n):
     if(not((m, n) in BINOMIAL_DICT)):
         BINOMIAL_DICT[(m, n)] = np.math.factorial(m) // (np.math.factorial(n) * np.math.factorial(m - n))
     return BINOMIAL_DICT.get((m, n))
-        
-        
+
+
 def comb_2(m, n):
     if((m, n) in BINOMIAL_DICT):
         return BINOMIAL_DICT.get((m, n))
@@ -35,7 +35,7 @@ def comb_2(m, n):
     else:
         BINOMIAL_DICT[(m, n)] = comb_2(m - 1, n - 1) + comb_2(m - 1, n)
         return BINOMIAL_DICT.get((m, n))
-        
+
 def bernstein_rec(n, k, t):
     if((n, k) in RECURSIVE_BERNSTEIN_DICT):
         return RECURSIVE_BERNSTEIN_DICT.get((n, k))
@@ -48,48 +48,43 @@ def bernstein_rec(n, k, t):
     else:
         RECURSIVE_BERNSTEIN_DICT[(n, k)] = t * bernstein_rec(n - 1, k - 1, t) + (1 - t) * bernstein_rec(n - 1, k, t)
         return RECURSIVE_BERNSTEIN_DICT.get((n, k))
-        
-def deCasteljau(k, i, cp, t): 
-    if(k == 0): 
+
+def deCasteljau(k, i, cp, t):
+    if(k == 0):
         return cp[i]
     return deCasteljau(k - 1, i, cp, t) * t + deCasteljau(k - 1, i + 1, cp, t) * (1 - t)
-    
-def horner(n, P, t_array): 
-    #Partimos en dos partes: >1/2 y <1/2
+
+def horner(n, cp, t_array):
+    #Partimos en dos partes: <1/2 y >1/2
     t_1, t_2 = np.split(t_array,2)
-    
-    #Calculamos array de numeros combinatorios
-    combarray = np.zeros(n+1)
-    for i in range (n+1):
-        combarray[i] = binom(n,i)
-    
-    #Calculamos los coeficientes para el método de Horner
-    coeffs_1 = P.T*combarray.T
-    
+
+    #Calculamos los coeficientes para el método de Horner cuando t<1/2
+    coeffs_1 = [comb_2(n, i) * cp[i] for i in range(n+1)]
     #Aplicamos Horner y multiplicamos por (1-t)^n
-    horner_11 = np.polyval(coeffs_1[:, 0], t_1/(1-t_1))*((1-t_1)**n)
-    horner_12 = np.polyval(coeffs_1[:, 1], t_1/(1-t_1))*((1-t_1)**n)
-    coeffs_2 = P[::-1].T*combarray.T
-    horner_21 = np.polyval(coeffs_2[:, 0], (1-t_2)/t_2)*(t_2**n)
-    horner_22 = np.polyval(coeffs_2[:, 1], (1-t_2)/t_2)*(t_2**n)
-    
-    return np.asarray([np.concatenate((horner_11, horner_21)), np.concatenate((horner_12, horner_22))])
-    
+    horner_1 = np.polyval(coeffs_1, t_1 / (1 - t_1)) * (1 - t_1)**n
+    #Calculamos los coeficientes para el método de Horner cuando t>1/2
+    coeffs_2 = [comb_2(n, i) * cp[::-1][i] for i in range(n+1)]
+    #Aplicamos Horner y multiplicamos por t^n
+    horner_2 = np.polyval(coeffs_2, (1 - t_2) / t_2) * t_2**n
+
+    return np.concatenate([horner_1, horner_2])
+
 def polyeval_bezier(P, num_points, algorithm):
+    n = P.size
     t_array = np.linspace(0, 1, num_points)
-    if (algorithm == 'direct'):
-        return comb(n, k) * t_array**k * (1 - t_array)**(n-k)
-    else if (algorithm == 'recursive'):
-        bernstein_rec(n, k, t_array)
-    else if (algorithm == 'horner'):
-        pass
-    else if (algorithm == 'deCasteljau'):
-        return deCasteljau(k, i, P, t_array)
+    if(algorithm == 'direct'):
+        return np.sum(P[k] * comb(n, k) * t_array ** k * (1 - t_array) ** (n - k) for k in range(n+1))
+    elif(algorithm == 'recursive'):
+        return np.sum(P[k] * bernstein_rec(n, k, t_array) for k in range(n+1))
+    elif(algorithm == 'horner'):
+        return horner(n, P, t_array)
+    elif(algorithm == 'deCasteljau'):
+        return deCasteljau(n, 0, P, t_array)
 
 def bezier_subdivision(P, k, epsilon, lines=False):
     '''
-    Metodo de subdivision. 
-    
+    Metodo de subdivision.
+
     P np.array de dimensión (num_points, dim).
     El entero k indicará el número de subdivisiones.
     Epsilon será el umbral de parada, que mide cuán cercana a una recta está la curva.
@@ -101,7 +96,7 @@ def bezier_subdivision(P, k, epsilon, lines=False):
 def backward_differences_bezier(P, m, h=None):
     '''
     Método de diferencias "hacia atrás".
-    
+
     Evaluará la curva de Bézier en los puntos de la forma h*k para k=0,...,m. Si h=None entonces h=1/m.
     '''
     pass
