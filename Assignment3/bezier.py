26008c74083ff6dba3bf85aae19194c2dfc2b9e1
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2016-04-29 19:58:14 +0200

    The light at the end of the tunnel

--- Assignment3/bezier.py
+++ Assignment3/bezier.py
@@ -19,26 +19,47 @@ can be found at Prautzsch-Bohm-Paluszny, chapters 2 and 3.
 
 import numpy as np
 
+
 BINOMIAL_DICT = dict()
 RECURSIVE_BERNSTEIN_DICT = dict()
 
+
 def polyeval_bezier(P, num_points, algorithm):
-    """
-    This function returns an array with the values of a Bezier curve
-    evaluated in the points given by the parameter 'numpoints'.
+    """This function returns an array with the values of a Bezier curve
+    evaluated in the number of points given by the parameter 'points_num'.
 
-    The option 'direct' will force a direct evaluation of Bernstein's polynomials,
-    'recursive' will calculate Bernstein's polynomials using its recursive formulae,
-    'horner' will use Horner's method in the evaluation of these polynomials, and finally
-    'deCasteljau' will evaluate the curve using De Casteljau's Algorithm.
+    The option 'direct' will force a direct evaluation of Bernstein's
+    polynomials, 'recursive' will calculate Bernstein's polynomials
+    using its recursive formulae, 'horner' will use Horner's method in
+    the evaluation of these polynomials, and finally 'deCasteljau'
+    will evaluate the curve using De Casteljau's algorithm.
 
-    Example
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    num_points
+        Number of points in [0, 1] to evaluate.
+    algorithm
+        Method to construct the Bezier curve.
+
+    Returns
     -------
-    P = [[-39, -71, -54], [82, -39, 1],  [-14, -89, -61],
-         [-50,  25, -78], [26,  40, 51], [-43, -81,  78]
-    polyeval_bezier(P, 100, 'horner')
-    Q = [[0.1, .8], [1,.2], [0,.1], [.3,.9]]
-    polyeval_bezier(Q, 50, 'direct')
+    result
+        Result of the evaluation: the curve.
+
+    Examples
+    --------
+    Introduce some control points:
+        >>> P = [[-39, -71, -54], [82, -39, 1],  [-14, -89, -61],
+                 [-50,  25, -78], [26,  40, 51], [-43, -81,  78]]
+    And try Horner, for instance:
+        >>> curve_1 = polyeval_bezier(P, 100, 'horner')
+    Or with some other points:
+        >>> Q = [[0.1, .8], [1,.2], [0,.1], [.3,.9]]
+    Go for the direct evaluation:
+        >>> curve_2 = polyeval_bezier(Q, 50, 'direct')
+
     """
     P = np.array(P)
     t_array = np.linspace(0, 1, num_points)
@@ -53,6 +74,16 @@ def polyeval_bezier(P, num_points, algorithm):
 
 
 def direct(cPoints, t_array):
+    """Direct evaluation using Bernstein's polynomials.
+
+    Parameters
+    ----------
+    cPoints
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
     # There are n+1 points of dimension dim on P
     n = np.size(cPoints, 0) - 1
     return np.sum(np.outer(cPoints[i].T,\
@@ -61,10 +92,40 @@ def direct(cPoints, t_array):
 
 
 def comb(m, n):
+    """Direct evaluation of the binomial coefficient (m, n),
+    with the help of a dictionary BINOMIAL_DICT.
+
+    Note
+    ----
+    The direct method is used here because it looks faster than
+    using the recursive formulae. It is implemented below, just in case.
+
+    Parameters
+    ----------
+    m
+        Upper value of the binomial coefficient.
+    n
+        Lower value of the binomial coefficient.
+
+    """
     if(not((m, n) in BINOMIAL_DICT)):
         BINOMIAL_DICT[(m, n)] = np.math.factorial(m) /\
                                (np.math.factorial(n) * np.math.factorial(m - n))
     return BINOMIAL_DICT.get((m, n))
+
+
+#def comb(m, n):
+#    """Recursive evaluation of the binomial coefficient (m, n),
+#    with the help of a dictionary BINOMIAL_DICT.
+#
+#    Parameters
+#    ----------
+#    m
+#        Upper value of the binomial coefficient.
+#    n
+#        Lower value of the binomial coefficient.
+#
+#    """
 #    if((m, n) in BINOMIAL_DICT):
 #        return BINOMIAL_DICT.get((m, n))
 #    elif(m == n or n == 0):
@@ -75,19 +136,41 @@ def comb(m, n):
 #        return BINOMIAL_DICT.get((m, n))
 
 
-def recursive(P, t):
+def recursive(P, t_array):
+    """Compute the curve using Bernstein's polynomials recursive formulae.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
     # There are n+1 points of dimension dim on P
     n, dim = P.shape - np.array([1, 0])
-    bezier = [np.sum(P[k][0] * bernstein_rec(n, k, t)\
+    bezier = [np.sum(P[k][0] * bernstein_rec(n, k, t_array)\
               for k in range(n+1))]
     for i in range(1, dim):
         bezier = np.concatenate((bezier,\
-                                 [np.sum(P[k][i] * bernstein_rec(n, k, t)\
+                                 [np.sum(P[k][i] * bernstein_rec(n, k, t_array)\
                                  for k in range(n+1))]))
     return bezier.T
 
 
 def bernstein_rec(n, k, t):
+    """Recursive formulae for computing the curve using Bernstein's polynomials.
+
+    Parameters
+    ----------
+    n
+        Column of the recursive structure.
+    k
+        Diagonal of the recursive structure.
+    t
+        Array of points in [0, 1] to evaluate.
+
+    """
     if((n, k) in RECURSIVE_BERNSTEIN_DICT):
         return RECURSIVE_BERNSTEIN_DICT.get((n, k))
     elif(k == -1 or k == n + 1):
@@ -103,6 +186,16 @@ def bernstein_rec(n, k, t):
 
 
 def horner(cPoints, t_array):
+    """Horner's method for the evaluation of Bernstein's polynomials.
+
+    Parameters
+    ----------
+    cPoints
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
     N = t_array.shape[0]
     # There are n+1 points of dimension dim on P
     n, dim = cPoints.shape - np.array([1, 0])
@@ -127,6 +220,16 @@ def horner(cPoints, t_array):
 
 
 def deCasteljau(P, t):
+    """Evaluation of the curve using De Casteljau's algorithm.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    t
+        Array of points in [0, 1] to evaluate.
+
+    """
     # There are n+1 points of dimension dim on P
     n, dim = np.shape(P) - np.array([1, 0])
     N = np.shape(t)[0]
@@ -145,15 +248,33 @@ def deCasteljau(P, t):
 
 
 def bezier_subdivision(P, k, epsilon, lines=False):
-    """
-    This function implements subdivision method
-    Integer parameter k indicates the number of subdivision, and epsilon
-    is the stop threshold, which measures how close to a straight line is the curve.
+    """This function implements subdivision method.
 
-    The function will return an array containing the sequence of points given by the resulting
-    Bezier polygons.
+    Integer parameter k indicates the number of subdivision
+    whereas epsilon is the stop threshold, which measures how
+    close to a straight line the curve is.
 
-    If lines = True, it will only return the succesion of exterms, wih no intermediate points.
+    The function will return an array containing the sequence of
+    points given by the resulting Bezier polygons.
+
+    If lines=True, it will only return the sequence of endpoints,
+    with no intermediate points.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    k
+        Number of subdivision.
+    epsilon
+        Threshold.
+    lines
+        Return only the sequence of endpoints, or intermediate points as well.
+
+    Returns
+    -------
+    result
+        Points representing Bezier's polygons.
 
     Example
     -------
@@ -186,13 +307,20 @@ def bezier_subdivision(P, k, epsilon, lines=False):
 
 
 def deCasteljau_subdivision(P):
-    """
+    """Variation of De Casteljau's method implement beforehand.
+
+    Here we return the diagonal of the matrix method and the last row.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
 
     """
-    n = P.shape[0] - 1
-    dim = P.shape[1]
+    # There are n+1 points of dimension dim on P
+    n, dim = P.shape - np.array([1, 0])
     bij = np.empty((n + 1, n + 1, dim))
-    bij[0,:,:] = P
+    bij[0, :, :] = P
     for i in xrange (1, n + 1):
         bij[i,:,:] = bij[i - 1, :, :] * 0.5 +\
                      np.vstack((np.zeros((1, dim)), bij[i - 1, :n, :])) * 0.5
@@ -200,18 +328,44 @@ def deCasteljau_subdivision(P):
 
 
 def backward_differences_bezier(P, m, h=None):
-    """
-    This function will evaluate Bezier's curve at points of the form h * k for k = 0, ..., m.
-    If h = None then h = 1/m.
-    The function uses the method of backward differences explained in class.
+    """This function will evaluate Bezier's curve at
+    points of the form h * k for k = 0, ..., m.
+    If h = None then h will be 1 / m.
+    The function uses the method of backward differences
+    explained at page 31 in the book mentioned above.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    m
+        Number of points for the evaluation minus one.
+    h
+        Separation between evaluations of the curve. Optional.
+
+    Returns
+    -------
+    p0, ..., pm
+        The m+1 points of the Bezier's curve
 
     Example
     -------
-    num_points = 100
-    h = 0.05
-    m = 100
-    P = [[-90, 29, 51], [-32, 80, -15], [-50, -40, -91], [-35, 93, 68], [-58, -97, 21]]
-    result = backward_differences_bezier(P, k, epsilon, True)
+    Evaluation of a curve given by a polynomial:
+        >>> polygon = np.array([[2, 26], [-60, -20], [69, -64], [25, 67]])
+        >>> m = 10
+        >>> result = backward_differences_bezier(poligono, 10)
+    The Bezier points will be:
+        >>> result = [[  2.00000000e+00   2.60000000e+01]
+                     [ -1.12340000e+01   1.24330000e+01]
+                     [ -1.51920000e+01   2.40000000e-02]
+                     [ -1.20580000e+01  -1.01890000e+01]
+                     [ -4.01600000e+00  -1.71680000e+01]
+                     [  6.75000000e+00  -1.98750000e+01]
+                     [  1.80560000e+01  -1.72720000e+01]
+                     [  2.77180000e+01  -8.32100000e+00]
+                     [  3.35520000e+01   8.01600000e+00]
+                     [  3.33740000e+01   3.27770000e+01]
+                     [  2.50000000e+01   6.70000000e+01]]
 
     """
     if h == None:
