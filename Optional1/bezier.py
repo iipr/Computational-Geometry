5c9d351ceac49931be776fe38a70fec257bbbf20
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2016-05-21 21:10:26 +0200

    Added optional assignment, interactive bezier curves with intersection (soon)

---
+++ Optional1/bezier.py
@@ -0,0 +1,408 @@
+"""This module provides different ways for obtaining Bezier curves.
+
+polyeval_bezier provides 4 ways to do this:
+using a direct evaluation of Bernstein's polynomials,
+using Bernstein's polynomials and its recursive formulae,
+using Horner's method in the evaluation of Bernstein's polynomials,
+or using De Casteljau's Algorithm.
+
+bezier_subdivision implements subdivision method.
+
+backward_differences_bezier uses backward differences method.
+
+Note
+----
+More information about Bezier curves and these methods
+can be found at Prautzsch-Bohm-Paluszny, chapters 2 and 3.
+
+"""
+
+from __future__ import division
+import numpy as np
+
+
+BINOMIAL_DICT = dict()
+RECURSIVE_BERNSTEIN_DICT = dict()
+
+
+def polyeval_bezier(P, num_points, algorithm):
+    """This function returns an array with the values of a Bezier curve
+    evaluated in the number of points given by the parameter 'points_num'.
+
+    The option 'direct' will force a direct evaluation of Bernstein's
+    polynomials, 'recursive' will calculate Bernstein's polynomials
+    using its recursive formulae, 'horner' will use Horner's method in
+    the evaluation of these polynomials, and finally 'deCasteljau'
+    will evaluate the curve using De Casteljau's algorithm.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    num_points
+        Number of points in [0, 1] to evaluate.
+    algorithm
+        Method to construct the Bezier curve.
+
+    Returns
+    -------
+    result
+        Result of the evaluation: the curve.
+
+    Examples
+    --------
+    Introduce some control points:
+        >>> P = [[-39, -71, -54], [82, -39, 1],  [-14, -89, -61],
+                 [-50,  25, -78], [26,  40, 51], [-43, -81,  78]]
+    And try Horner, for instance:
+        >>> curve_1 = polyeval_bezier(P, 100, 'horner')
+    Or with some other points:
+        >>> Q = [[0.1, .8], [1,.2], [0,.1], [.3,.9]]
+    Go for the direct evaluation:
+        >>> curve_2 = polyeval_bezier(Q, 50, 'direct')
+
+    """
+    # Transform into a numpy.array, just in case
+    P = np.array(P)
+    # Array of points in [0, 1] to evaluate the curve.
+    t_array = np.linspace(0, 1, num_points)
+    if algorithm == 'direct':
+        return direct(P, t_array)
+    elif algorithm == 'recursive':
+        return recursive(P, t_array)
+    elif algorithm == 'horner':
+        return horner(P, t_array)
+    else:
+        return deCasteljau(P, t_array)
+
+
+def direct(cPoints, t_array):
+    """Direct evaluation using Bernstein's polynomials.
+
+    Parameters
+    ----------
+    cPoints
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
+    # There are n+1 points of dimension dim on P
+    n = np.size(cPoints, 0) - 1
+    return np.sum(np.outer(cPoints[i].T,\
+                           comb(n, i) * t_array ** i * (1 - t_array) ** (n - i))\
+                           for i in range(n + 1)).T
+
+
+def comb(m, n):
+    """Direct evaluation of the binomial coefficient (m, n),
+    with the help of a dictionary BINOMIAL_DICT.
+
+    Note
+    ----
+    The direct method is used here because it looks faster than
+    using the recursive formulae. It is implemented below, just in case.
+
+    Parameters
+    ----------
+    m
+        Upper value of the binomial coefficient.
+    n
+        Lower value of the binomial coefficient.
+
+    """
+    # First check in the dictionary, and add it if it is not there
+    if(not((m, n) in BINOMIAL_DICT)):
+        BINOMIAL_DICT[(m, n)] = np.math.factorial(m) /\
+                               (np.math.factorial(n) * np.math.factorial(m - n))
+    return BINOMIAL_DICT.get((m, n))
+
+
+#def comb(m, n):
+#    """Recursive evaluation of the binomial coefficient (m, n),
+#    with the help of a dictionary BINOMIAL_DICT.
+#
+#    Parameters
+#    ----------
+#    m
+#        Upper value of the binomial coefficient.
+#    n
+#        Lower value of the binomial coefficient.
+#
+#    """
+#    if((m, n) in BINOMIAL_DICT):
+#        return BINOMIAL_DICT.get((m, n))
+#    elif(m == n or n == 0):
+#        BINOMIAL_DICT[(m, n)] = 1
+#        return 1
+#    else:
+#        BINOMIAL_DICT[(m, n)] = comb(m - 1, n - 1) + comb(m - 1, n)
+#        return BINOMIAL_DICT.get((m, n))
+
+
+def recursive(P, t_array):
+    """Compute the curve using Bernstein's polynomials recursive formulae.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
+    # There are n+1 points of dimension dim on P
+    n, dim = P.shape - np.array([1, 0])
+    # Compute the curve, by coordinates
+    bezier = [np.sum(P[k][0] * bernstein_rec(n, k, t_array)\
+              for k in range(n+1))]
+    for i in range(1, dim):
+        bezier = np.concatenate((bezier,\
+                                 [np.sum(P[k][i] * bernstein_rec(n, k, t_array)\
+                                 for k in range(n+1))]))
+    return bezier.T
+
+
+def bernstein_rec(n, k, t):
+    """Recursive formulae for computing the curve using Bernstein's polynomials.
+
+    Parameters
+    ----------
+    n
+        Column of the recursive structure.
+    k
+        Diagonal of the recursive structure.
+    t
+        Array of points in [0, 1] to evaluate.
+
+    """
+    # First check in the dictionary
+    if((n, k) in RECURSIVE_BERNSTEIN_DICT):
+        return RECURSIVE_BERNSTEIN_DICT.get((n, k))
+    elif(k == -1 or k == n + 1):
+        RECURSIVE_BERNSTEIN_DICT[(n, k)] = 0
+        return 0
+    elif(n == 0 and k == 0):
+        RECURSIVE_BERNSTEIN_DICT[(n, k)] = 1
+        return 1
+    # Add it if it is not in the dictionary
+    else:
+        RECURSIVE_BERNSTEIN_DICT[(n, k)] = t * bernstein_rec(n - 1, k - 1, t) +\
+                                           (1 - t) * bernstein_rec(n - 1, k, t)
+        return RECURSIVE_BERNSTEIN_DICT.get((n, k))
+
+
+def horner(cPoints, t_array):
+    """Horner's method for the evaluation of Bernstein's polynomials.
+
+    Parameters
+    ----------
+    cPoints
+        n-dimensional control points.
+    t_array
+        Array of points in [0, 1] to evaluate.
+
+    """
+    N = t_array.shape[0]
+    # There are n+1 points of dimension dim on P
+    n, dim = cPoints.shape - np.array([1, 0])
+
+    # Coefficients from the polynomial to evaluate when t<1/2
+    coeffs_1 = np.asarray([comb(n, i) * cPoints[n - i,:]\
+                          for i in range(n + 1)])
+    # Apply Horner's method and multiply times (1-t)^n
+    t_1 = t_array[:int(N / 2)]
+    horner_1 = [np.polyval(coeffs_1[:, j], t_1 / (1 - t_1)) * (1 - t_1)**n\
+               for j in range(dim)]
+
+    # Coefficients from the polynomial to evaluate when t>1/2
+    coeffs_2 = np.asarray([comb(n, i) * cPoints[i,:]\
+                          for i in range(n+1)])
+    t_2 = t_array[int(N / 2):]
+    # Apply Horner's method and multiply times t^n
+    horner_2 = [np.polyval(coeffs_2[:, j], (1 - t_2) / t_2) * t_2**n\
+               for j in range(dim)]
+
+    return np.hstack((horner_1, horner_2)).T
+
+
+def deCasteljau(P, t):
+    """Evaluation of the curve using the De Casteljau's algorithm.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    t
+        Array of points in [0, 1] to evaluate.
+
+    """
+    # There are n+1 points of dimension dim on P
+    n, dim = np.shape(P) - np.array([1, 0])
+    N = np.shape(t)[0]
+    # 3-D matrix to save De Casteljau's method values 
+    matrix = np.zeros((N, n+1, dim))
+
+    # Control polygon initialization
+    matrix[:, 1:] = t[:, np.newaxis, np.newaxis] * P[1:,:] +\
+              (1 - t)[:, np.newaxis, np.newaxis] * P[:n,:]
+    #Apply the De Casteljau's method
+    for i in range(2, n+1):
+        matrix[:, i:] = t[:, np.newaxis, np.newaxis] * matrix[:, i:] +\
+                  (1 - t)[:, np.newaxis, np.newaxis] * matrix[:, (i-1):n]
+
+    return matrix[:, n]
+
+
+def bezier_subdivision(P, k, epsilon, lines=False):
+    """This function implements subdivision method.
+
+    Integer parameter k indicates the number of subdivision
+    whereas epsilon is the stop threshold, which measures how
+    close to a straight line the curve is.
+
+    The function will return an array containing the sequence of
+    points given by the resulting Bezier polygons.
+
+    If lines=True, it will only return the sequence of endpoints,
+    with no intermediate points.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    k
+        Number of subdivision.
+    epsilon
+        Threshold.
+    lines
+        Return only the sequence of endpoints, or intermediate points as well.
+
+    Returns
+    -------
+    result
+        Points representing Bezier's polygons.
+
+    Example
+    -------
+    num_points = 100
+    epsilon = 0.01
+    k = 10
+    P = [[21, -17, -27], [92, -46, -36], [-14, -66, -75],
+         [72, -41, -89], [49, -37,  83], [-48,  63,  66], [-94, -51, 71]]
+    result = bezier_subdivision(np.array(P), k, epsilon, True)
+
+    """
+    # There are n+1 points of dimension dim on P
+    n, dim = P.shape - np.array([1, 0])
+
+    # Compute threshold for subvdivision method
+    delta2_b = np.diff(P, n=2, axis=0)
+    threshold = np.max(np.linalg.norm(delta2_b, axis=1))
+
+    # Check whether lines is true or not, and threshold condition
+    if lines and n*(n - 1) / 8 * threshold < epsilon:
+        return np.array([P[0], P[-1]])
+
+    if k == 0 or threshold < epsilon:
+        return P
+
+    # Call for De Casteljau, different from the one before
+    P0, P1 = deCasteljau_subdivision(P)
+    return np.vstack((bezier_subdivision(P0, k-1, epsilon)[:-1,:],\
+                      bezier_subdivision(P1, k-1, epsilon)))
+
+
+def deCasteljau_subdivision(P):
+    """Variation of De Casteljau's method implement beforehand.
+
+    Here we return the diagonal of the matrix method and the last row.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+
+    """
+    # There are n+1 points of dimension dim on P
+    n, dim = P.shape - np.array([1, 0])
+    bij = np.empty((n + 1, n + 1, dim))
+    bij[0, :, :] = P
+    for i in xrange (1, n + 1):
+        bij[i,:,:] = bij[i - 1, :, :] * 0.5 +\
+                     np.vstack((np.zeros((1, dim)), bij[i - 1, :n, :])) * 0.5
+    return np.diagonal(bij).T, bij[:, n][::-1]
+
+
+def backward_differences_bezier(P, m, h=None):
+    """This function will evaluate Bezier's curve at
+    points of the form h * k for k = 0, ..., m.
+    If h = None then h will be 1 / m.
+    The function uses the method of backward differences
+    explained at page 31 in the book mentioned above.
+
+    Parameters
+    ----------
+    P
+        n-dimensional control points.
+    m
+        Number of points for the evaluation minus one.
+    h
+        Separation between evaluations of the curve. Optional.
+
+    Returns
+    -------
+    p0, ..., pm
+        The m+1 points of the Bezier's curve
+
+    Example
+    -------
+    Evaluation of a curve given by a polynomial:
+        >>> polygon = np.array([[2, 26], [-60, -20], [69, -64], [25, 67]])
+        >>> m = 10
+        >>> result = backward_differences_bezier(polygon, m)
+    The Bezier points will be:
+        >>> result = [[  2.00000000e+00   2.60000000e+01]
+                      [ -1.12340000e+01   1.24330000e+01]
+                      [ -1.51920000e+01   2.40000000e-02]
+                      [ -1.20580000e+01  -1.01890000e+01]
+                      [ -4.01600000e+00  -1.71680000e+01]
+                      [  6.75000000e+00  -1.98750000e+01]
+                      [  1.80560000e+01  -1.72720000e+01]
+                      [  2.77180000e+01  -8.32100000e+00]
+                      [  3.35520000e+01   8.01600000e+00]
+                      [  3.33740000e+01   3.27770000e+01]
+                      [  2.50000000e+01   6.70000000e+01]]
+
+    """
+    if h == None:
+        h = 1/m
+    #First obtain polynomial's dimensions
+    n, d = np.shape(P) - np.array([1, 0])
+    t_array = np.arange(0, (n + 1)*h, h)
+
+    #Inicialice with Horner's method
+    p_init = horner(P, t_array)
+    
+    #Compute the forward differences
+    forward = np.zeros((n + 1, d))
+    file_n = np.zeros((n + 1,d))
+    file_n[0] = p_init[n]  
+    forward[1:] = p_init[1:] - p_init[:n]
+    file_n[1] = forward[n]
+    
+    for i in range(2,n + 1):
+        forward[i:] = forward[i:] - forward[(i - 1):n]
+        file_n[i] = forward[n]
+
+    #Finally compute the backward recursion
+    backward = np.zeros((m - n + 1, n + 1, d))
+
+    #We inverse the book's matrix to use cumsum
+    backward[:] = file_n[::-1] 
+    
+    for i in range(1, m-n+1):
+        backward[i:] = np.cumsum(backward[i:], axis=1)
+
+    #Return the p0...pM points obtained
+    return np.vstack((p_init, backward[1:,n]))
---
+++ Optional1/interactive_bezier.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 7 12:51:23 2016
+
+@author: Jagui, Iker & Kike
+"""
+
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import RadioButtons
+import bezier as bz
+
+
+class Interactive_Bezier():
+    def __init__(self):
+        self.figure, self.axes = plt.subplots(figsize=(10, 10))
+        self.axes.set_xlim(-10, 10)
+        self.axes.set_ylim(-10, 10)
+        self.touched_circle = None
+        self.circles1 = []
+        self.circles2 = []
+        self.line1 = None
+        self.line2 = None
+        self.curve1 = None
+        self.curve2 = None
+        self.bezier1 = None
+        self.bezier2 = None
+        self.cPoints1 = None
+        self.cPoints2 = None
+        self.curveindex = 1
+        self.index = -1 # indice del circulo tocado
+        self.n1 = -1 # numero de puntos del poligono de control de la primera curva menos 1
+        self.n2 = -1 # numero de puntos del poligono de control de la segunda curva menos 1
+        self.cid_press = self.figure.canvas.mpl_connect('button_press_event', self.click_event)
+        self.cid_move = self.figure.canvas.mpl_connect('motion_notify_event', self.motion_event)
+        self.cid_release = self.figure.canvas.mpl_connect('button_release_event', self.release_event)
+        self.cid_close = self.figure.canvas.mpl_connect('close_event', self.close_event)
+        self.figure.canvas.set_window_title('Interactive Bezier curves and intersection')
+
+        # Independent window for selecting the algorithm that computes the curves
+        self.algorithm = 'direct'
+        self.figButtons = plt.figure(figsize=(2.2,2.2))
+        self.figButtons.add_subplot(111)
+        self.checkAlgorithm = RadioButtons(self.figButtons.axes[0],('Direct', 'Recursive', 'Horner', 'DeCasteljau', \
+                                                                'Subdivision', 'Backward'), activecolor='g')
+        self.checkAlgorithm.on_clicked(self.radio_click)
+        self.cid_close2 = self.figButtons.canvas.mpl_connect('close_event', self.close_event)
+        self.checkAlgorithm.canvas.set_window_title('Algorithm selector')
+
+    def close_event(self, event):
+        print('Bye, bye world!')
+        self.figure.canvas.mpl_disconnect(self.cid_press)
+        self.figure.canvas.mpl_disconnect(self.cid_move)
+        self.figure.canvas.mpl_disconnect(self.cid_release)
+        self.figure.canvas.mpl_disconnect(self.close_event)
+#        plt.close()
+        plt.close(self.figure)
+        plt.close(self.figButtons)
+    
+    def radio_click(self, label):
+        # Redraw depending on the selected method
+        if label == 'Direct':
+            self.algorithm = 'direct'
+            print 'Direct mode'
+        elif label == 'Recursive':
+            print 'Recursive mode'
+            self.algorithm = 'recursive'
+        elif label == 'Horner':
+            print 'Horner mode'
+            self.algorithm = 'horner'
+        elif label == 'DeCasteljau':
+            print 'DeCasteljau algorithm'
+            self.algorithm = 'deCasteljau'
+        elif label == 'Subdivision':
+            print 'Subdivision algorithm'
+            self.algorithm = 'subdivision'
+        elif label == 'Backward':
+            print 'Backward differences mode'
+            self.algorithm = 'backward'
+        self.computeCurve()
+        self.drawBezier()
+        self.figure.canvas.draw()
+#        plt.draw()
+
+    def calculateIndex(self):
+        for i in range(self.n1 + 1):
+#            print self.cPoints1[i][0], self.cPoints1[i][1]
+            if (self.cPoints1[i][0] == self.x0 and self.cPoints1[i][1] == self.y0): 
+                self.index = i
+                self.curveindex = 1
+
+        for i in range(self.n2 + 1):
+#            print self.cPoints2[i][0], self.cPoints2[i][1]
+            if (self.cPoints2[i][0] == self.x0 and self.cPoints2[i][1] == self.y0): 
+                self.index = i
+                self.curveindex = 2
+
+    def computeCurve(self):
+        if self.curveindex == 1:
+            self.n1 = np.size(self.cPoints1, 0) - 1
+            if self.algorithm in ['direct', 'recursive', 'horner', 'deCasteljau']:
+                num_points = 1000
+                self.curve1 = bz.polyeval_bezier(self.cPoints1, num_points, self.algorithm)
+            elif self.algorithm == 'subdivision':
+                k = 5
+                epsilon = 0.01
+                self.curve1 = bz.bezier_subdivision(self.cPoints1, k, epsilon)
+            elif self.algorithm == 'backward':
+#                h = 0.05
+                m = 100
+                self.curve1 = bz.backward_differences_bezier(self.cPoints1, m)
+        else:
+            self.n2 = np.size(self.cPoints2, 0) - 1
+            if self.algorithm in ['direct', 'recursive', 'horner', 'deCasteljau']:
+                self.curve2 = bz.polyeval_bezier(self.cPoints2, 1000, self.algorithm)
+            elif self.algorithm == 'subdivision':
+                k = 5
+                epsilon = 0.01
+                self.curve2 = bz.bezier_subdivision(self.cPoints2, k, epsilon)
+            elif self.algorithm == 'backward':
+#                h = 0.05
+                m = 100
+                self.curve2 = bz.backward_differences_bezier(self.cPoints2, m)
+
+    def swapIndex(self):
+        if self.curveindex == 1:
+            self.curveindex = 2
+        else: # curveindex == 2
+            self.curveindex = 1
+
+    def computeControlPolygon(self, event):
+        if self.curveindex == 1:
+            c = plt.Circle((event.xdata, event.ydata), radius=0.2, color='lightblue')
+            if self.cPoints1 == None:
+                self.cPoints1 = np.asarray([[event.xdata, event.ydata]])
+            else:
+                self.cPoints1 = np.append(self.cPoints1, [[event.xdata, event.ydata]], axis=0)
+            # Now we can compute the curve given cPoints1
+            self.computeCurve()
+            # Add the (graphic) circle to the list circles2
+            self.circles1.append(c)
+        else:
+            c = plt.Circle((event.xdata, event.ydata), radius=0.2, color='lightgreen')
+            if self.cPoints2 == None:
+                self.cPoints2 = np.asarray([[event.xdata, event.ydata]])
+            else:
+                self.cPoints2 = np.append(self.cPoints2, [[event.xdata, event.ydata]], axis=0)
+            # Now we can compute the curve given cPoints2
+            self.computeCurve()
+            # Add the (graphic) circle to the list circles2
+            self.circles2.append(c)
+        self.axes.add_artist(c)
+
+    def drawBezier(self):
+        if self.bezier1 == None and self.n1 == 2:
+            self.bezier1 = plt.Line2D(self.curve1[:, 0], self.curve1[:, 1], color='blue')
+            self.axes.add_line(self.bezier1)
+        elif self.n1 > 2:
+            self.bezier1.set_data(self.curve1[:, 0], self.curve1[:, 1])
+         
+        if self.bezier2 == None and self.n2 == 2:
+            self.bezier2 = plt.Line2D(self.curve2[:, 0], self.curve2[:, 1], color='green')
+            self.axes.add_line(self.bezier2)
+        elif self.n2 > 2:
+            self.bezier2.set_data(self.curve2[:, 0], self.curve2[:, 1])
+
+    def drawControlPolygon(self):
+        if len(self.circles1) > 0:
+            if self.line1 == None:
+                self.line1 = plt.Line2D(*zip(*map(lambda x: x.center, self.circles1)), color='lightblue')
+                self.axes.add_line(self.line1)
+            else:
+                self.line1.set_data(*zip(*map(lambda x: x.center, self.circles1)))
+
+        if len(self.circles2) > 0:
+            if self.line2 == None:
+                self.line2 = plt.Line2D(*zip(*map(lambda x: x.center, self.circles2)), color='lightgreen')
+                self.axes.add_line(self.line2)
+            else:
+                self.line2.set_data(*zip(*map(lambda x: x.center, self.circles2)))
+
+    def click_event(self, event):
+        # If we click outside the axes, nothing happens
+        if event.inaxes != self.axes: 
+            return
+
+        # If we click on the right button, we change from one curve to the other
+        # in order to add more control points (modify control points is independent of this)
+        if event.button == 3:
+            self.swapIndex()
+            return
+
+        # For convenience, we allow the user to close the program by clicking with the scroll button
+        elif event.button == 2:
+            self.close_event(event)
+            return
+        
+        # If we clicked with the left button, we save the event
+        self.initial_event = event
+        # Check if we have clicked on one of the control points
+        for c in self.axes.artists:
+            if c.contains(event)[0]:
+                self.touched_circle = c
+                self.x0, self.y0 = c.center
+                # Compute the index of the touched circle in the corresponding cPoints array
+                self.calculateIndex() 
+                return
+
+        # Create and draw the circle and add it to the corresponding cPoints array
+        self.computeControlPolygon(event)
+
+        # If the number of points is at least one, we draw the control polygon
+        self.drawControlPolygon()
+
+        # If the number of points is 3 we can create the (graphic) bezier curve
+        # If it is greater than 3, we simply recalculate it
+        self.drawBezier()
+
+        self.figure.canvas.draw()
+        
+    def motion_event(self, event):
+        if self.touched_circle == None:
+            return
+        if event.inaxes == self.axes:
+            dx = event.xdata - self.initial_event.xdata
+            dy = event.ydata - self.initial_event.ydata
+            self.touched_circle.center = self.x0 + dx, self.y0 + dy
+            if self.curveindex == 1:
+                self.line1.set_data(*zip(*map(lambda x: x.center, self.circles1)))
+            else:
+                self.line2.set_data(*zip(*map(lambda x: x.center, self.circles2)))
+                
+            self.figure.canvas.draw()
+        
+    def release_event(self, event):
+        if self.touched_circle == None:
+            return
+        if self.curveindex == 1:
+            self.cPoints1[self.index] = np.asarray(self.touched_circle.center)
+            if self.n1 > 1:
+                self.computeCurve()
+                self.bezier1.set_data(self.curve1[:, 0], self.curve1[:, 1])
+        else:
+            self.cPoints2[self.index] = np.asarray(self.touched_circle.center)
+            if self.n2 > 1:
+                self.computeCurve()
+                self.bezier2.set_data(self.curve2[:, 0], self.curve2[:, 1])
+            
+        self.touched_circle = None
+        self.figure.canvas.draw()
+        
+if __name__ == "__main__" :
+    interactive_Bezier = Interactive_Bezier()
+    plt.show()
